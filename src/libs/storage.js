b553a8a0d7a48757bf075eccd3e0f2ce6ba21c72
Yudi454 <lucasyudi445@gmail.com>
2023-11-28 15:07:52 -0300

    Agregue un log para encontrar un error

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -4,6 +4,8 @@ const multer = require("multer")
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, path.resolve(__dirname, '../../public/Images'))
+      console.log(path.resolve(__dirname, '../../public/Images'));
+
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
