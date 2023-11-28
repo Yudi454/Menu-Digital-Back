64d46a5c3ef7b194836849ec51ea56253a897dc3
Yudi454 <lucasyudi445@gmail.com>
2023-11-28 14:44:56 -0300

    Quite un log

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -7,7 +7,6 @@ const getUsers = async (req, res) => {
   console.log("pase get users");
   try {
     const users = await usuarioModel.find();
-    console.log(users);
     res.status(200).json(users);
   } catch (error) {
     console.log("Error:", error);
